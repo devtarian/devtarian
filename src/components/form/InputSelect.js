ae48c9bf95e4c021d683f36938ddc8bbcd91f541
kyungyoonha <gkb10a@naver.com>
2020-12-18 17:06:41 +0900

    엥러: Merge에러 수정

---
+++ src/components/form/InputSelect.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InputSelect = ({ className, label, name, value, onChange, options }) => {
+  return (
+    <Wrap className={className}>
+      {label && <label>{label}</label>}
+      <select name={name} value={value} onChange={onChange}>
+        {options.map((item, idx) => (
+          <option key={idx} value={item.title}>
+            {item.title}
+          </option>
+        ))}
+      </select>
+    </Wrap>
+  );
+};
+
+export default InputSelect;
+
+const Wrap = styled.div`
+  select {
+    width: 100%;
+    font: inherit;
+    padding: 9px;
+    border-radius: 4px;
+    border: 1px solid ${(props) => props.theme.gray[1]};
+
+    option {
+      font-size: 1.1rem;
+    }
+  }
+`;
