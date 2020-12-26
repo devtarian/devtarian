bbed5700709c55d638d52d539dbe8720a376f0b1
kyungyoonha <gkb10a@naver.com>
2020-12-26 20:31:43 +0900

    기능변경: axios baseURL 수정

--- src/Service/apis/default.js
+++ src/Service/apis/default.js
@@ -6,6 +6,6 @@ if (token) {
 }
 
 export const defaultApi = axios.create({
-  //baseURL: 'https://asia-northeast3-project-devtarian.cloudfunctions.net/api',
-  baseURL: 'http://localhost:5001/project-devtarian/asia-northeast3/api',
+  baseURL: 'https://asia-northeast3-project-devtarian.cloudfunctions.net/api',
+  //baseURL: 'http://localhost:5001/project-devtarian/asia-northeast3/api',
 });
