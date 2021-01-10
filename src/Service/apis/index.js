6b94ef8c3f696cf8757eeb033c082807091890ce
kyungyoonha <gkb10a@naver.com>
2021-01-10 12:45:48 +0900

    기능추가: service wikiAPI

--- src/Service/apis/index.js
+++ src/Service/apis/index.js
@@ -1,6 +1,7 @@
 import { api as authApi } from './auth';
 import { api as postsApi } from './posts';
+import { api as wikiApi } from './wiki';
 
-const apis = { authApi, postsApi };
+const apis = { authApi, postsApi, wikiApi };
 
 export default apis;
