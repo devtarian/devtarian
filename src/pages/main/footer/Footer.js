421347395583af9260c92777150ccadd99c8c2c7
ddayooung <dimmaculatey@gmail.com>
2021-01-20 20:51:37 +0900

    기능추가: footer link 연결

--- src/pages/main/footer/Footer.js
+++ src/pages/main/footer/Footer.js
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
+import kyungyoonHa from '../../../images/kyungyoonha.png';
+import dayoungLee from '../../../images/dayounglee.png';
 
 const Footer = () => {
   return (
     <Wrap>
       <ul>
         <li className="linkItem">
-          <a href="https://github.com/daayooung">
-            <img src="http://placehold.it/40x40.png?text=A" alt="GIT HUB" />
+          <a href="https://github.com/kyungyoonha">
+            <img src={kyungyoonHa} alt="kyungyoonHa's Repository" />
+            <strong>kyungyoonHa</strong>
           </a>
         </li>
         <li className="linkItem">
-          <a href="https://twofivezero.tistory.com/">
-            <img src="http://placehold.it/40x40.png?text=A" alt="개발블로그" />
+          <a href="https://github.com/daayooung">
+            <img src={dayoungLee} alt="dayoungLee's Repository" />
+            <strong>dayoungLee</strong>
           </a>
         </li>
       </ul>
@@ -24,21 +28,29 @@ const Footer = () => {
 
 const Wrap = styled.section`
   width: 100%;
+  margin-top: 16rem;
 
   ul {
     text-align: center;
   }
   .linkItem {
     display: inline-block;
+    width: 80px;
+    height: 60px;
     margin: 15rem 10px 0;
-
+    margin: 0 auto;
     img {
+      width: 40px;
+      height: 40px;
       border-radius: 50%;
+    }
+    strong {
+      font-size: 10px;
     }
   }
 
   p {
-    margin: 2rem 0;
+    margin: 1.2rem 0 2rem;
     text-align: center;
     font-size: 12px;
   }
